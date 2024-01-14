0b6fe3ce55e60a498c53a2530d1d3d9289c8f498
lukasz123 <exlar@DESKTOP-ASJ7D84.(none)>
2024-01-14 17:16:06 +0100

    Set city name

---
+++ src/test/java/HotelSearch.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class HotelSearch {
+    @Test
+    public void searchHotel(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
+        driver.findElement(By.xpath("//*[@id='select2-drop']/div/input")).sendKeys("Dubai");
+        driver.findElement(By.xpath("//div[@id='select2-drop']//span[text()='Dubai']")).click();
+
+
+
+    }
+}
